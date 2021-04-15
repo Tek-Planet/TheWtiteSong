c7b38963555a04dc77f2e44b0b7ef8123c113c3b
abiola <abiolaolalekan39@gmail.com>
2021-04-15 14:27:19 +0100

    Created My Topic Screen

---
+++ src/screens/Community/MyTopicScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+
+const MyTopicScreen = () => {
+  return <ScrollView></ScrollView>;
+};
+
+export default MyTopicScreen;
