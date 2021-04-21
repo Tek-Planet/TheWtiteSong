29a8d3e82f91cf907c0393ec42de5e947a6e8cd2
abiola <abiolaolalekan39@gmail.com>
2021-04-21 11:54:46 +0100

    created sub-component ticket.js and added it to supportScreen.js

--- src/screens/SupportScreen.js
+++ src/screens/SupportScreen.js
@@ -1,11 +1,121 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import Message from '../components/Message';
+import Ticket from '../components/Ticket';
 
 const SupportScreen = () => {
   return (
-    <ScrollView>
+    <ScrollView style={{backgroundColor: '#fff'}}>
+      <Message header="Home > Support > Trouble Ticket View" />
       <View>
-        <Text>Welcome to Support Screen</Text>
+        <Text
+          style={{
+            textAlign: 'center',
+            marginTop: 10,
+            fontWeight: 'bold',
+            fontSize: 18,
+          }}>
+          All Tickets
+        </Text>
+        <Ticket isOpen="Open" />
+        <Ticket isOpen="Open" />
+        <Ticket isOpen="Closed" />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            backgroundColor: 'grey',
+            paddingTop: 15,
+            paddingBottom: 15,
+            borderRadius: 20,
+          }}>
+          <View>
+            <Text
+              style={{
+                fontSize: 16,
+                color: '#fff',
+                fontWeight: 'bold',
+                marginBottom: 20,
+              }}>
+              Subject:
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: '#fff',
+                fontWeight: 'bold',
+              }}>
+              From:
+            </Text>
+          </View>
+          <View>
+            <Text
+              style={{
+                fontSize: 16,
+                color: 'yellow',
+                marginBottom: 20,
+              }}>
+              Storage Space Low
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: 'yellow',
+              }}>
+              System Administrator
+            </Text>
+          </View>
+          <View>
+            <Text
+              style={{
+                fontSize: 16,
+                color: '#fff',
+                fontWeight: 'bold',
+                marginBottom: 20,
+              }}>
+              Date:
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: '#fff',
+                fontWeight: 'bold',
+              }}>
+              Time:
+            </Text>
+          </View>
+          <View>
+            <Text
+              style={{
+                fontSize: 16,
+                color: 'yellow',
+                marginBottom: 20,
+              }}>
+              15/01/2020
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: 'yellow',
+              }}>
+              11:59 PM, EST
+            </Text>
+          </View>
+        </View>
+        <View style={{padding: 15}}>
+          <Text style={{marginBottom: 10, fontSize: 16}}>Message:</Text>
+          <Text style={{fontSize: 16}}>
+            Dear Subscriber, Your online storage limit has only 32MB Free.
+            Kindly remove some songs repository or purchase additional storage
+            space
+          </Text>
+        </View>
       </View>
     </ScrollView>
   );
