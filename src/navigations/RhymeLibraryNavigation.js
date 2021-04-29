61de37f9176ecbae0604b9de0c01c64d7d50dc94
abiola <abiolaolalekan39@gmail.com>
2021-04-29 15:31:42 +0100

    Done with the UI Design for Rhyming Tool

--- src/navigations/RhymeLibraryNavigation.js
+++ src/navigations/RhymeLibraryNavigation.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
 import {createStackNavigator} from '@react-navigation/stack';
 import RhymeLibraryScreen from '../screens/RhymeLibrary/RhymeLibraryScreen';
 import SongRhymingToolScreen from '../screens/RhymeLibrary/SongRhymingToolScreen';
@@ -8,9 +6,9 @@ import SongRhymingToolScreen from '../screens/RhymeLibrary/SongRhymingToolScreen
 const Stack = createStackNavigator();
 
 const RhymeLibraryNavigation = ({navigation}) => (
-  <Stack.Navigator headerMode={'none'} initialRouteName="RhymeLibrary">
-    <Stack.Screen name="RhymeLibrary" component={RhymeLibraryScreen} />
+  <Stack.Navigator headerMode={'none'} initialRouteName="SongRhymingTool">
     <Stack.Screen name="SongRhymingTool" component={SongRhymingToolScreen} />
+    <Stack.Screen name="RhymeLibrary" component={RhymeLibraryScreen} />
   </Stack.Navigator>
 );
 
