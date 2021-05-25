4187c04af27550dbbef3ff1591d113150000eb42
tekplanet <tekplanet@tekplanet.tekplanet>
2021-05-25 16:49:24 +0100

    25th may update

--- src/screens/lyrics/SongTemplateEditor.js
+++ src/screens/lyrics/SongTemplateEditor.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import Message from '../../components/Message';
 import SongVersesPosition from '../../components/lyrics/SongVersesPosition';
 import SongTemplateHeader from '../../components/SongTemplateHeader';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Button from '../../components/SaveButton';
 
 export function MySongsEditor({navigation, route}) {
   const {songInfo} = route.params;
@@ -16,6 +23,13 @@ export function MySongsEditor({navigation, route}) {
           header={'Lyrics Manager > Song Template Editor'}
         />
         <SongTemplateHeader songInfo={songInfo} />
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('LyricsNav', {screen: 'Lyrics'});
+          }}>
+          <Text>Add song element</Text>
+        </TouchableOpacity>
+
         <View
           style={{marginTop: 10, borderBottomWidth: 3, borderColor: '#AC1C1C'}}
         />
